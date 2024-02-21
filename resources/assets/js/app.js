9eabad7e71b99f0e7fd5fedd70c8009edb78ad46
Rogine Bawiga <bawigarogine02@gmai.com>
2024-02-21 09:03:34 +0800

    Update: 02/21/2024

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,4 +6,61 @@ function navbarOffcanvasClose(navbar) {
   navbar.style.display = "none";
 }
 
-export default {navbarOffcanvasOpen , navbarOffcanvasClose};
+function modalShow(data){
+  let candidates = getCandidates()
+  const div = document.createElement("div")
+  candidates = candidates[data.details]
+  data.modal.style.display = "none"
+
+  div.innerHTML = `
+    <div class="modal-content">
+      <h3>${candidates.fname} ${candidates.lname}</h3>
+    </div>
+    `
+    
+  if (data.modal.style.display == "none") {
+    data.modal.style.display = "block"
+  }else{
+    data.modal.style.display = "none"
+  }
+
+  data.modal.appendChild(div)
+  console.log(data.modal);
+}
+
+function getCandidates(){
+  let candidateDetails = [
+    {
+      fname: 'oking',
+      lname: 'Marapo',
+      quote: 'This is a quote just i don\'t know',
+      img: "../assets/img/avatar.webp",
+      course: 'COMPUTER'
+    },
+    {
+      fname: 'dfdfd',
+      lname: 'dfdfd',
+      quote: 'This is a quote just i don\'t know',
+      img: "../assets/img/avatar.webp",
+      course: 'CRIMINOLOGY'
+    },
+    {
+      fname: 'okifdfdfdng',
+      lname: 'dfdfdfd',
+      quote: 'This is a quote just i don\'t know',
+      img: "../assets/img/avatar.webp",
+      course: 'COMMERCE'
+    },
+    {
+      fname: 'fghfhfh',
+      lname: 'fghfhfhf',
+      quote: 'This is a quote just i don\'t know',
+      img: "../assets/img/avatar.webp",
+      course: 'EDUCATION'
+    },
+  ];
+
+  return candidateDetails;
+}
+
+export default {navbarOffcanvasOpen , navbarOffcanvasClose, modalShow, getCandidates};
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,12 +1,55 @@
-import appFunctions from './app.js';
+import app from "./app.js";
+import appFunctions from "./app.js";
 
-let navbarToggler = document.getElementById("navbar-toggler")
-let navbarClose = document.getElementById("navbar-close")
-let navbar = document.querySelector("nav")
+let navbarToggler = document.getElementById("navbar-toggler");
+let navbarClose = document.getElementById("navbar-close");
+let navbar = document.querySelector("nav");
 
 navbarToggler.onclick = () => {
-    appFunctions.navbarOffcanvasOpen(navbar)
-}
+  appFunctions.navbarOffcanvasOpen(navbar);
+};
+
 navbarClose.onclick = () => {
-    appFunctions.navbarOffcanvasClose(navbar)
+  appFunctions.navbarOffcanvasClose(navbar);
+};
+
+let candidateProfileBtns = document.querySelectorAll(".view-profile");
+let candidateModal = document.querySelectorAll("candidate-card");
+
+for (let i = 0; i < candidateProfileBtns.length; i++) {
+  candidateProfileBtns[i].onclick = () => {
+    appFunctions.modalShow(candidateProfileBtns[i]);
+  };
+}
+
+// CANDIDATES - candidates.html
+
+let candidates = document.getElementById("candidates")
+const candidateDetails = appFunctions.getCandidates()
+
+
+for (let i = 0; i < candidateDetails.length; i++) {
+  const div = document.createElement("div");
+
+  div.innerHTML = `
+    <div class="candidate-card">
+    <img src="${candidateDetails[i].img}" alt="candidate-1">
+    <h3>${candidateDetails[i].fname}${candidateDetails[i].lname}</h3>
+    <p>${candidateDetails[i].quote}</p>
+    <p class="course">${candidateDetails[i].course}</p>
+    <button type="button" class="view-profile">View Profile</button>
+    </div>
+`;
+
+  candidates.appendChild(div);
+}
+
+const viewProfileBtn = document.querySelectorAll(".view-profile")
+const modal = document.getElementById("candidate-modal")
+
+for (let i = 0; i < viewProfileBtn.length; i++) {
+  viewProfileBtn[i].onclick = () => {
+    const data = {details: i, modal: modal}
+    appFunctions.modalShow(data)
+  }
 }
