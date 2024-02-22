e246bf6aed627d481a293162697f58dd7e4844cb
Rogine Bawiga <bawigarogine02@gmai.com>
2024-02-22 11:59:38 +0800

    Update: 02/22/2024 11:59am

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,88 +6,159 @@ function navbarOffcanvasClose(navbar) {
   navbar.style.display = "none";
 }
 
-function getCandidates(){
+function getCandidates() {
   let candidateDetails = [
     {
-      fname: 'Lawrence',
-      lname: 'Marapo',
-      quote: 'This is a quote just i don\'t know',
+      fname: "Lawrence",
+      lname: "Marapo",
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur magni id, sit molestiae harum incidunt libero ex doloremque perspiciatis laboriosam.",
       img: "../assets/img/avatar.webp",
-      course: 'COMPUTER'
+      course: "COMPUTER",
+      partyList: "OKING1",
     },
     {
-      fname: 'Mark Daniel',
-      lname: 'Santillan',
-      quote: 'This is a quote just i don\'t know',
+      fname: "Mark Daniel",
+      lname: "Santillan",
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur magni id, sit molestiae harum incidunt libero ex doloremque perspiciatis laboriosam.",
       img: "../assets/img/avatar.webp",
-      course: 'CRIMINOLOGY'
+      course: "CRIMINOLOGY",
+      partyList: "OKING1",
     },
     {
-      fname: 'Samuel',
-      lname: 'Chavez',
-      quote: 'This is a quote just i don\'t know',
+      fname: "Samuel",
+      lname: "Chavez",
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur magni id, sit molestiae harum incidunt libero ex doloremque perspiciatis laboriosam.",
       img: "../assets/img/avatar.webp",
-      course: 'COMMERCE'
+      course: "COMMERCE",
+      partyList: "OKING1",
     },
     {
-      fname: 'Vince Kenneth',
-      lname: 'Jumawan',
-      quote: 'This is a quote just i don\'t know',
+      fname: "Vince Kenneth",
+      lname: "Jumawan",
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur magni id, sit molestiae harum incidunt libero ex doloremque perspiciatis laboriosam.",
       img: "../assets/img/avatar.webp",
-      course: 'EDUCATION'
+      course: "EDUCATION",
+      partyList: "OKING1",
     },
     {
-      fname: 'Earl Bryan',
-      lname: 'Espinosa',
-      quote: 'This is a quote just i don\'t know',
+      fname: "Earl Bryan",
+      lname: "Espinosa",
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur magni id, sit molestiae harum incidunt libero ex doloremque perspiciatis laboriosam.",
       img: "../assets/img/avatar.webp",
-      course: 'EDUCATION'
+      course: "EDUCATION",
+      partyList: "OKING1",
     },
     {
-      fname: 'John Ralph',
-      lname: 'Sayson',
-      quote: 'This is a quote just i don\'t know',
+      fname: "John Ralph",
+      lname: "Sayson",
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur magni id, sit molestiae harum incidunt libero ex doloremque perspiciatis laboriosam.",
       img: "../assets/img/avatar.webp",
-      course: 'EDUCATION'
+      course: "EDUCATION",
+      partyList: "OKING1",
     },
     {
-      fname: 'Dame Ruds',
-      lname: 'Escaña',
-      quote: 'This is a quote just i don\'t know',
+      fname: "Dame Ruds",
+      lname: "Escaña",
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur magni id, sit molestiae harum incidunt libero ex doloremque perspiciatis laboriosam.",
       img: "../assets/img/avatar.webp",
-      course: 'EDUCATION'
+      course: "EDUCATION",
+      partyList: "OKING1",
     },
     {
-      fname: 'Jeo Marc',
-      lname: 'Fernandez',
-      quote: 'This is a quote just i don\'t know',
+      fname: "Jeo Marc",
+      lname: "Fernandez",
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur magni id, sit molestiae harum incidunt libero ex doloremque perspiciatis laboriosam.",
       img: "../assets/img/avatar.webp",
-      course: 'EDUCATION'
+      course: "EDUCATION",
+      partyList: "OKING1",
     },
     {
-      fname: 'Brent',
-      lname: 'Carallas',
-      quote: 'This is a quote just i don\'t know',
+      fname: "Brent",
+      lname: "Carallas",
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur magni id, sit molestiae harum incidunt libero ex doloremque perspiciatis laboriosam.",
       img: "../assets/img/avatar.webp",
-      course: 'EDUCATION'
+      course: "EDUCATION",
+      partyList: "OKING1",
     },
   ];
 
   return candidateDetails;
 }
 
-function viewCandidateProfile(card, candidateContainer, data){
+function addCandidateCard(candidates) {
+  const candidateDetails = getCandidates();
+  for (let i = 0; i < candidateDetails.length; i++) {
+    const div = document.createElement("div");
 
-    const div = document.createElement("div")
-    
     div.innerHTML = `
-      <div class="candidate-card-details">
-      ;dshhfjkdshkfjhsdhfkshksh
+      <div class="candidate-card">
+      <div>
+      <img src="${candidateDetails[i].img}" alt="candidate-1">
+      <span>Party-list : ${candidateDetails[i].partyList}</span>
       </div>
-    `
-    card.appendChild(div)
+      <h3>${candidateDetails[i].fname} ${candidateDetails[i].lname}</h3>
+      <p class="testimony">${candidateDetails[i].quote}</p>
+      <p class="course">${candidateDetails[i].course}</p>
+      <button type="button" class="view-profile">View Profile</button>
+      </div>
+  `;
 
-    candidates.remove()
+    candidates.appendChild(div);
+  }
 }
 
-export default {navbarOffcanvasOpen , navbarOffcanvasClose, getCandidates, viewCandidateProfile};
+function viewCandidateProfile(card, candidateContainer, data) {
+  const div = document.createElement("div");
+
+  div.innerHTML = `
+      <div class="candidate-card-details">
+        <div class="container">
+          <div class="col">
+          <img src="${data.img}" />
+          <p>${data.course}</p>
+          <span>Party-list : ${data.partyList}</span>
+        </div>
+        <div class="col">
+          <h1>${data.fname} ${data.lname}</h1>
+          <p class="subtitle">Party-list : ${data.partyList}</p>
+          <p class="testimony"><i>'</i> ${data.quote} <i>'</i></p>
+          <div>
+          <button type="button" id="back-btn"><i class="fa fa-arrow-left-long"></i> Back</button>
+          <button type="button" id="vote-btn">Vote Now</button>
+          </div>
+        </div>
+        </div>
+      </div>
+    `;
+  card.appendChild(div);
+
+  candidateContainer.remove();
+
+  let backBtn = document.getElementById("back-btn")
+
+  backBtn.onclick = () => {
+    
+  }
+}
+
+function backToCandidates(card, candidateProfile) {
+    console.log(card);
+}
+
+export default {
+  navbarOffcanvasOpen,
+  navbarOffcanvasClose,
+  getCandidates,
+  viewCandidateProfile,
+  addCandidateCard,
+  backToCandidates
+};
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -25,32 +25,23 @@ for (let i = 0; i < candidateProfileBtns.length; i++) {
 // CANDIDATES - candidates.html
 
 let candidates = document.getElementById("candidates");
-const candidateDetails = appFunctions.getCandidates();
 
-for (let i = 0; i < candidateDetails.length; i++) {
-  const div = document.createElement("div");
 
-  div.innerHTML = `
-    <div class="candidate-card">
-    <img src="${candidateDetails[i].img}" alt="candidate-1">
-    <h3>${candidateDetails[i].fname} ${candidateDetails[i].lname}</h3>
-    <p>${candidateDetails[i].quote}</p>
-    <p class="course">${candidateDetails[i].course}</p>
-    <button type="button" class="view-profile">View Profile</button>
-    </div>
-`;
+  appFunctions.addCandidateCard(candidates)
 
-  candidates.appendChild(div);
-}
+  const candidateDetails = appFunctions.getCandidates();
+  const viewCandidateBtn = document.querySelectorAll(".view-profile");
+  const candidatesMain = document.querySelector("#candidates-main .wrapper")
 
-const viewCandidateBtn = document.querySelectorAll(".view-profile");
-const candidatesMain = document.querySelector("#candidates-main .wrapper")
+  for (let i = 0; i < viewCandidateBtn.length; i++) {
+    viewCandidateBtn[i].onclick = () => {
 
-for (let i = 0; i < viewCandidateBtn.length; i++) {
-  viewCandidateBtn[i].onclick = () => {
-    
+      appFunctions.viewCandidateProfile(candidatesMain,candidates,candidateDetails[i])
 
-    appFunctions.viewCandidateProfile(candidatesMain,candidates,candidateDetails[i])
-
+    }
   }
-}
+
+
+
+
+
